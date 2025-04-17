583f519dc25a86ea2e7527e56fb420ada60f37fe
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-04-17 19:28:10 +0530

    added dynamic port

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,15 @@ dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
 
-    console.log("app started listening.")
+    console.log(`App started listening on port ${PORT}`);
 })
 
 app.get("/", (req:Request, res: Response)=>{
